efa321545e59e4c912b23b82a45a8e26395de751
Jivin Chugh <jivinchugh18@gmail.com>
2025-04-12 18:59:37 -0400

    changed the layout to stack+tab navigation for better UI

--- Car-Owner-app/App.js
+++ Car-Owner-app/App.js
@@ -5,41 +5,78 @@ used this documentation to solve the problem by setting headerBackVisible to fal
  */
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import LoginScreen from "./screens/LoginScreen";
 import MyListings from "./screens/MyListings";
 import CreateListings from "./screens/CreateListings";
 import { Button } from "react-native";
-
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabContainerComponent = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="MyListings"
+      screenOptions={({ route }) => ({
+        headerShown: true,
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "MyListings") {
+            return (
+              <FontAwesome6 name="car" size={24} color="purple" />
+            );
+          } else if (route.name === "CreateListings") {
+            return (
+              <MaterialIcons name="car-rental" size={30} color="purple" />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen 
+        name="MyListings" 
+        component={MyListings} 
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <Button
+              onPress={() => navigation.navigate("CreateListings")}
+              title="Add New"
+              color="purple"
+              style={{ marginRight: 10 }}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen 
+        name="CreateListings" 
+        component={CreateListings}
+        options={{ title: "Add New Car" }} 
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="LoginScreen">
         <Stack.Screen
-          name="Login Screen"
+          name="LoginScreen"
           component={LoginScreen}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-          name="MyListings"
-          component={MyListings}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <Button
-                onPress={() => navigation.navigate("CreateListings")}
-                title="Add New"
-                color="purple"
-              />
-            ),
-          })}
+          name="TabContainerComponent"
+          component={TabContainerComponent}
+          options={{
+            headerShown: false,
+            headerBackVisible: false,
+          }}
         />
-        <Stack.Screen 
-          name="CreateListings" 
-          component={CreateListings}
-        />
-        </Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
