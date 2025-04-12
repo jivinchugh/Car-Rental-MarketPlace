3f0ff5f96b0d918a7df5484063a9ae5cc919e017
Jivin Chugh <jivinchugh18@gmail.com>
2025-04-12 14:20:49 -0400

    used stack instead of native-stack

--- Car-Owner-app/App.js
+++ Car-Owner-app/App.js
@@ -1,12 +1,17 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+/* Problem faced - was able to go back from search to login
+headerShown false was not preventing going back to login screen
+https://reactnavigation.org/docs/native-stack-navigator#headerbackvisible
+used this documentation to solve the problem by setting headerBackVisible to false
+ */
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "./screens/LoginScreen";
 import MyListings from "./screens/MyListings";
 import CreateListings from "./screens/CreateListings";
 import { Button } from "react-native";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
